cebc063c9bfebfd87640ceb4ac9bf0f37eaf14e4
matthewstifler <mtwstflr@gmail.com>
2016-06-02 20:04:34 +0300

    tweaked dtm, added more advanced analysis

--- code/wall-comments-processing.R
+++ code/wall-comments-processing.R
@@ -1,6 +1,9 @@
 require(stringr)
 require(tm)
 require(RWeka)
+require(slam)
+require(wordcloud)
+require(mallet)
 
 comm.df$text = gsub("(\\[)(id\\d*)(\\|)([A-Za-zА-Яа-я]*])", " ", comm.df$text) #removing references in comments
 comm.df$text = gsub("(https://)(\\S*)", " ", comm.df$text) #remove URLs
@@ -8,13 +11,20 @@ comm.df$text = gsub("[^А-Яа-я]", " ", comm.df$text)
 
 corp <- Corpus(VectorSource(comm.df$text), readerControl=list(language="ru", encoding="UTF-8"))
 corp <- tm_map(corp, content_transformer(tolower))
-corp <- tm_map(corp, removeWords, c(stopwords("ru"), "очень", "просто", "вообще", "хотя", "вроде"))
+corp <- tm_map(corp, removeWords, c(stopwords("ru"), "очень", "просто", "вообще", "хотя", "вроде", "это"))
 corp <- tm_map(corp, stripWhitespace)
 
-dtm.control <- list(weighting=weightTf, stemming=FALSE, bounds=list(global=c(5,2000)))
+dtm.control <- list(weighting=weightTf, stemming=FALSE, bounds=list(global=c(5,Inf)))
 dtm<-DocumentTermMatrix(corp, control=dtm.control)
 
 #attempt at extracting ngrams
 BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 4, max = 4))
 dtm.bi <- DocumentTermMatrix(corp, control = list(tokenize = BigramTokenizer))
 options(mc.cores=1) #you just gotta
+
+dtm = removeSparseTerms(dtm, 0.9999)
+word.freq <- sort(slam::col_sums(dtm, na.rm = T), decreasing = T)
+
+png("~/anime-soc-ling/plots/wordcloud-dobrochan.png")
+wordcloud(words = names(word.freq), freq = word.freq, scale = c(5,0.1),min.freq = 3, max.words = 200, random.order = F, colors = pal)
+dev.off()
